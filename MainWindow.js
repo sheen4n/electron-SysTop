0ba754602bd00c930cbe0edf8f46b1b737464250
Goh Sheen An <sheen.an.goh@gmail.com>
2020-06-26 10:09:08 +0800

    first commit

---
+++ MainWindow.js
@@ -0,0 +1,24 @@
+const { BrowserWindow } = require('electron');
+
+class MainWindow extends BrowserWindow {
+  constructor(file, isDev) {
+    super({
+      title: 'SysTop',
+      width: isDev ? 1000 : 355,
+      height: 600,
+      icon: './assets/icons/icon.png',
+      resizable: isDev ? true : false,
+      show: false,
+      opacity: 0.9,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
+
+    this.loadFile(file);
+
+    if (isDev) this.webContents.openDevTools();
+  }
+}
+
+module.exports = MainWindow;
